48f5f0aa80e535ab0436bc1ba3a0b64df22a7786
siddeshwar.korwar <sidkorwar52@gmail.com>
2025-06-16 15:50:10 +0530

    Testing commit from intellij id

--- codingproblems/src/cz/Demo.java
+++ codingproblems/src/cz/Demo.java
@@ -3,15 +3,11 @@ package cz;
 import java.sql.Timestamp;
 
 public class Demo {
-	
-	
-	public static void main(String[] args) {
-		
-		
-		System.out.println(new Timestamp(System.currentTimeMillis()));
-		
-		
-		
-	}
+
+    public static void main(String[] args) {
+
+        System.out.println(new Timestamp(System.currentTimeMillis()));
+
+    }
 
 }
